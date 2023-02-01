50436958db88482b4b059c4a8c0fc0803bbe4146
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-02-01 20:19:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distinct Coloring/distinct-coloring.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    public static long distinctColoring(int N, int[]r, int[]g, int[]b){
+        //code here
+        long x = r[0];
+        long y = g[0];
+        long z = b[0];
+        for(int i=1;i<N;i++){
+            long x1= x;
+            long y1 = y;
+            long z1 =z;
+            x = r[i] +Math.min(y1,z1);
+            y = g[i]+Math.min(x1,z1);
+            z = b[i]+Math.min(x1,y1);
+        }
+        return Math.min(x,Math.min(y,z));
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input[] = read.readLine().trim().split("\\s+");
+            int[]r=new int[N];
+            for(int i=0;i<N;i++)
+                r[i]=Integer.parseInt(input[i]);
+            input = read.readLine().trim().split("\\s+");
+            int[]g=new int[N];
+            for(int i=0;i<N;i++)
+                g[i]=Integer.parseInt(input[i]);
+            input = read.readLine().trim().split("\\s+");
+            int[]b=new int[N];
+            for(int i=0;i<N;i++)
+                b[i]=Integer.parseInt(input[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.distinctColoring(N, r, g, b));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
