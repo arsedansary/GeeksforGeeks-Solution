70f8b2e6b4f69efeb4307f33615a48740a4d774c
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-12-31 18:25:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/New Year Resolution/new-year-resolution.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] coins = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            boolean res = obj.isPossible(N, coins);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean isPossible(int n, int[] coins) {
+        return recursion(coins, 0, 0, n);
+    }
+    
+    static boolean recursion(int arr[], int currSum, int currInd, int n){
+        if((currSum % 24 == 0 || currSum%20 == 0 || currSum == 2024) && currSum != 0)return true;
+        if(currInd >= n)return false;
+        return (recursion(arr, currSum+arr[currInd], currInd+1, n) || recursion(arr, currSum, currInd+1, n));
+    }
+}
+        
